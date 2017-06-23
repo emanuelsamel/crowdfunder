4928e0417312a63f332ec215c6e070896ec260b7
Don Cooper <don_cooper@hotmail.com>
2017-06-23 10:59:04 -0400

    Reward must be positive number is working

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,5 +1,13 @@
 class Reward < ActiveRecord::Base
   belongs_to :project
-  validates :dollar_amount, :description, presence: true 
+  validates :dollar_amount, :description, presence: true
+
+  validate :reward_dollar_amount_must_be_positive
+
+  def reward_dollar_amount_must_be_positive
+    if dollar_amount < 0
+      errors.add(:dollar_amount, " must be larger than zero.")
+    end
+  end
 
 end
